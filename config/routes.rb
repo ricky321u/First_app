5d3383603feea0352d9ef67ebf93fa11a86ff672
ricky321u <ricky321u@gmail.com>
2016-10-04 23:33:52 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :users
+  root to:'root#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
